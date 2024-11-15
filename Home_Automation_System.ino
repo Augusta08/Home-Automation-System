c6978c9dd867916e7e25cfb782beee1a4202a278
Augusta <auguchinenye@gmail.com>
2024-11-15 19:05:48 +0100

    Corrected button numbers and removed case 3

--- Home_Automation_System.ino
+++ Home_Automation_System.ino
@@ -2,15 +2,12 @@
 
 #define IR_RECEIVE_PIN 8
 
-#define IR_BUTTON_1 22
-#define IR_BUTTON_2 25
-#define IR_BUTTON_3 13
-#define IR_BUTTON_4 12
-#define IR_BUTTON_5 24
+#define IR_BUTTON_1 16
+#define IR_BUTTON_2 17
+#define IR_BUTTON_3 18
 
 #define ledPin 13
 #define buzzer 12
-
 
 void setup() {
   Serial.begin(9600);
@@ -21,37 +18,31 @@ void setup() {
 
 void loop() {
   if (IrReceiver.decode()) {
-    delay(100);
     IrReceiver.resume();
     int command = IrReceiver.decodedIRData.command;
     switch (command) {
-      case IR_BUTTON_1: {
-        Serial.println("Pressed on button 1");
-        if (digitalRead(ledPin) == LOW) {
-          digitalWrite(ledPin, HIGH);
-        } else {
-          digitalWrite(ledPin, LOW);
+      case IR_BUTTON_1:
+        {
+          Serial.println("Pressed on button 1");
+          if (digitalRead(ledPin) == LOW) {
+            digitalWrite(ledPin, HIGH);
+          } else {
+            digitalWrite(ledPin, LOW);
+          }
+          break;
         }
-        break;
-      }
-      case IR_BUTTON_2: {
-        Serial.println("Pressed on button 2");
-        digitalWrite(buzzer, HIGH);
-        delay(250);
-        digitalWrite(buzzer, LOW);
-        break;
-      }
-      case IR_BUTTON_3: {
-        Serial.println("Pressed on button 3");
-        break;
-      }
-      case IR_BUTTON_4: {
-        Serial.println("Pressed on button play/pause");
-        break;
-      }
-      default: {
-        Serial.println("Button not recognized");
-      }
+      case IR_BUTTON_2:
+        {
+          Serial.println("Pressed on button 2");
+          digitalWrite(buzzer, HIGH);
+          delay(250);
+          digitalWrite(buzzer, LOW);
+          break;
+        }
+      default:
+        {
+          Serial.println("Button not recognized");
+        }
     }
   }
 }
